b8d4e9be28b99f961d79811bcaefcec265487f1d
MaximilianoBinetti <60740296+MaximilianoBinetti@users.noreply.github.com>
2020-02-06 11:13:36 -0300

    Primer Ejercicio
    Use alert, variables, y comenté.

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,16 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{    var primerPrecio; 
+	 var segundoPrecio;
+     var apellido= "nadie"; // se crea la variable apellido, esto se guarda en la memoria RAM y se autocompleta próximamente
+	 
+	 apellido= "Perez"; //le asigne el valor Pérez, un valor es literal si esta entre ""
+     //si se encuentra entre comillas es un valor literal
+	alert("tal cual");// alert es una funcion que recibe un texto y lo muestra
+	alert(apellido); //al agregar las barras se cancelan los comandos y se toman como comentarios
+	//alert("apellido");
+	//alert(); // sale vacío
+	//alert(primerPrecio); //indefinido, ya que no se le asigno ningún valor
+	//alert(preciopri); //si la variable no existe no podemos hacer nada
+}
+
