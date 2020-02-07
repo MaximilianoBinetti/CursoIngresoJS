b114879792c3472c542b0dc640bda795f42aeb9f
MaximilianoBinetti <60740296+MaximilianoBinetti@users.noreply.github.com>
2020-02-07 10:21:22 -0300

    Ejercicio 3
    Tomé valores por id, copiando valores del código HTML.

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{   var nombre 
+	nombre =document. getElementById('elNombre').value; //document sirve para acceder a todas las funcionalidades y características de este documento usando el punto
+	alert(nombre); //se pone .value para obtener el valor del cuadro de texto
+
+}
+
+
