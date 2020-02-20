76d8a54dc901d97f096322f0c3724f13f9386e77
MaximilianoBinetti <60740296+MaximilianoBinetti@users.noreply.github.com>
2020-02-20 09:45:02 -0300

    Instrucción Switch 2
    Usé switch y default.

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,29 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+alert (mesDelAño);
+
+switch(mesDelAño)
+{
+	case "Julio":
+	case "Agosto":
+		alert("Abrigate que hace frio.");
+		break; 
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Junio":
+		alert("Falta para el invierno");
+		break;
+	default:
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;		
+}
+
+
+
+
+}//FIN DE LA FUNCIÓN
