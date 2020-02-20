fe8a4e40f15e349b8c0373d0994c1bb931b969db
MaximilianoBinetti <60740296+MaximilianoBinetti@users.noreply.github.com>
2020-02-20 09:55:10 -0300

    Instrucción Switch 4
    Completo

---
+++ jsInstruccionSwitch(4).js
@@ -0,0 +1,28 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+
+switch(mesDelAño)
+{
+  case "Febrero":
+    alert("estes mes tiene 28 días");
+    break;
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+    	alert("Este mes tiene 30 días");
+    break;
+    default:
+    	alert("Este mes tiene 31 días");
+    	break;	 
+}
+	
+	
+
+
+
+}//FIN DE LA FUNCIÓN
